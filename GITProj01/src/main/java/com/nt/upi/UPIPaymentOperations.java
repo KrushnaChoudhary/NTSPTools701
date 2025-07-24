bf74920aee7ad3abc2e7afb45c30bf1360f44288
Krushna Choudhary <Krushnachoudhary28805@gmail.com>
2025-07-24 17:04:37 +0530

    Dev1-created UPI Payment feature

--- GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -2,4 +2,7 @@ package com.nt.upi;
 
 public class UPIPaymentOperations {
 
+	public String doPayment(long phoNo,double amount) {  
+		return amount+"amount is paid to phone number::"+phoNo; 
+	}
 }
