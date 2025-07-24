43c9b667c1a91e5035b46c21f22d9a82e5df01b1
Krushna Choudhary <Krushnachoudhary28805@gmail.com>
2025-07-24 17:08:45 +0530

    B1 code is reviewed by TL

--- GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -2,7 +2,8 @@ package com.nt.upi;
 
 public class UPIPaymentOperations {
 
+	//Payment feature
 	public String doPayment(long phoNo,double amount) {  
-		return amount+"amount is paid to phone number::"+phoNo; 
+		return amount+"amount is paid to @phone number::"+phoNo; 
 	}
 }
